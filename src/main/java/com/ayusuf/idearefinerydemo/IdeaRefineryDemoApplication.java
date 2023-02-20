d11cfdb773bfc2254bc0a1fe45819c838c79c243
Ammar Yusuf <amyusuf1@gmail.com>
2023-02-20 17:29:23 -0500

    Initial commit of Spring Boot Demo project for Idea Refinery.

---
+++ src/main/java/com/ayusuf/idearefinerydemo/IdeaRefineryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ayusuf.idearefinerydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaRefineryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaRefineryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayusuf/idearefinerydemo/IdeaRefineryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayusuf.idearefinerydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaRefineryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
