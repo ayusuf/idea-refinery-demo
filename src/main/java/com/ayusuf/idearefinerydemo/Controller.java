78bf8b56295843e686d499445ee3788d95bc7dec
Ammar Yusuf <amyusuf1@gmail.com>
2023-02-22 16:28:04 -0500

    Added Kafka topic, listener, and producer.

--- src/main/java/com/ayusuf/idearefinerydemo/Controller.java
+++ src/main/java/com/ayusuf/idearefinerydemo/Controller.java
@@ -1,13 +1,27 @@
 package com.ayusuf.idearefinerydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    String kafkaTopic = "topic1";
+
     @GetMapping("/")
     public String index() {
         return "Hello World";
     }
+
+    @GetMapping("/send")
+    public String send() {
+        kafkaTemplate.send(kafkaTopic, "Test");
+        return "Sent Test";
+    }
+
 }
